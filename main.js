00c60113707c3827c3536cb3f615c34226721cbd
yulianto <jooliantoo@gmail.com>
2024-05-12 10:01:15 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+var username = "admin";
+var password = "123456";
+var email = "2WjJi@example.com";
+console.log(username, password, email);
---
+++ main.ts
@@ -0,0 +1,4 @@
+let username = "admin";
+let password = "123456";
+let email = "2WjJi@example.com";
+console.log(username, password, email);
